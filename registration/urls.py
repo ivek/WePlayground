b1dc56569f68d633c563baa2b26cb48fd283f9bc
Ivek Martinez Herrera <ivekdelfin@gmail.com>
2020-04-05 01:02:04 -0600

    cambio de email y validacion no se repita

--- registration/urls.py
+++ registration/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import SignUpView, ProfileUpdate
+from .views import SignUpView, ProfileUpdate, EmailUpdate
 
 app_name='registration'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
     path('profile/', ProfileUpdate.as_view(), name="profile"),
+    path('profile/email/', EmailUpdate.as_view(), name="profile_email"),
 ]
 
 
--- registration/views.py
+++ registration/views.py
@@ -1,4 +1,4 @@
-from .forms import UserCreationFormWithEmail
+from .forms import UserCreationFormWithEmail, ProfileForm, EmailForm
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 from django import forms
@@ -27,11 +27,27 @@ class SignUpView(CreateView):
 
 @method_decorator(login_required, name= 'dispatch')
 class ProfileUpdate(UpdateView):
-    model = Profile
-    fields = ['avatar', 'bio', 'link']
+    form_class = ProfileForm
     success_url = reverse_lazy('registration:profile')
     template_name= 'registration/profile_form.html'
 
     def get_object(self):
         profile, created = Profile.objects.get_or_create(user= self.request.user)
         return profile
+
+@method_decorator(login_required, name= 'dispatch')
+class EmailUpdate(UpdateView):
+    form_class = EmailForm
+    success_url = reverse_lazy('registration:profile')
+    template_name= 'registration/profile_email_form.html'
+
+    def get_object(self):
+        return self.request.user
+
+    def get_form(self, form_class=None):
+        form = super(EmailUpdate, self).get_form()
+        #modificar en tiempo real
+        form.fields['email'].widget = forms.EmailInput(
+            attrs={'class':'form-control mb2', 'placeholder':'Email'})
+        return form
+        
